97944c45fea37589e800a93dec892936cb4a2062
abdallamfathy <abdallamfathy@gmail.com>
2022-10-26 19:26:37 +0200

    Make SongCard UI , add image , title , subtitle to it

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,42 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from "react-router-dom";
+import {PlayPause} from "./PlayPause"
+const SongCard = ({song}) => {  
+  const activeSong = "Test";
+  
+  const handlePauseClick = () => {    
+
+  }
+
+  const handlePlayClick = () => {
+    
+  }
+
+  return (
+  <div className="bg-white/5 w-[250px] bg-opacity-80 backdrop-blur-2xl rounded-lg animate-slideup cursor-pointer p-4">
+    <div className="relative h-56 group w-full ">
+      <div className={`absolute inset-0 justify-center items-center bg-opacity-50 group-hover:flex bg-black ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden' }`}>
+        <PlayPause 
+        song={song}
+        handlePause={handlePauseClick}
+        handlePlay={handlePlayClick}
+        />
+      </div>
+      <img src={song.images?.coverart} alt="song-img" />
+    </div>
+    <div className="flex flex-col mt-4">
+    <p className="font-semibold text-white truncate text-lg">
+    <Link to={`/songs/${song?.key}`}>
+      {song.title}
+      </Link>
+    </p>
+    <p className="text-sm truncate text-gray-300 mt-1">
+    <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : "/top-artists"}>
+      {song.subtitle}
+      </Link>
+    </p>
+    </div>
+  </div>
+  )
+  }
 
 export default SongCard;
