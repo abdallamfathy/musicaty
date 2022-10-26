f82833e0872ee3b9b337c568fafef7af0f246dbc
abdallamfathy <abdallamfathy@gmail.com>
2022-10-26 17:25:14 +0200

    Add Error component UI

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="flex  justify-center w-full items-center">
+    
+    <h1 className="text-white font-bold mt-2 text-2xl">Something went wrong, please try again later.</h1>
+  </div>
 );
 
 export default Error;
