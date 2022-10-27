5b17e9b258e249a38e433f4387aaa47c810f8a91
abdallamfathy <abdallamfathy@gmail.com>
2022-10-28 01:44:25 +0200

    Add song details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,10 +9,10 @@ const SongDetails = () => {
     console.log(songid);
     return (
         <div className="flex flex-col">
-            {/* <DetailsHeader
-            artistId={artistId}
+            <DetailsHeader
+            artistId={""}
             songData={songData}
-            /> */}
+            />
             <div className="mb-10">
                 <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
                 <div className="mt-5">
