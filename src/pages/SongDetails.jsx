09e378caead43162d52f190b0b49f2046f4b7ed2
abdallamfathy <abdallamfathy@gmail.com>
2022-10-28 01:01:34 +0200

    Fetch song data and lyrics from api

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,28 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { DetailsHeader } from "../components";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+
+const SongDetails = () => {
+
+    const {songid} = useParams()
+    const {data:songData , isFetching:isFetchingSongDetails} = useGetSongDetailsQuery(songid)
+    console.log(songid);
+    return (
+        <div className="flex flex-col">
+            {/* <DetailsHeader
+            artistId={artistId}
+            songData={songData}
+            /> */}
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+                <div className="mt-5">
+                    {songData?.sections[1].type === "LYRICS" ? songData?.sections[1].text.map((line,i) => (
+                        <p>{line}</p>
+                    )): <p>Sorry, no lyrics found!</p>}
+                </div>
+            </div>
+        </div>
+        )
+};
 
 export default SongDetails;
