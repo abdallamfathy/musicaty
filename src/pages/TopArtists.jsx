68e275bc823877ff3fe996852a32138b5c874d14
abdallamfathy <abdallamfathy@gmail.com>
2022-10-31 23:09:02 +0200

    make artist Card

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -11,7 +11,6 @@ const TopArtists = () => {
     return (
         <div className='flex flex-col'>
             <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>Top Artists
-            
             </h2>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.map((track) => (
