5a316db953568997b6c8434e17d15df64cfd61b5
abdallamfathy <abdallamfathy@gmail.com>
2022-10-31 22:52:54 +0200

    Make Top Artists

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Error from '../components/Error';
 import Loader from '../components/Loader';
