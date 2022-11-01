b908dfa1557ac358ef00055ee7f1a88825750a02
abdallamfathy <abdallamfathy@gmail.com>
2022-11-01 23:12:20 +0200

    make Search bar works

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import {FiSearch} from "react-icons/fi"
+import { useNavigate } from "react-router-dom";
 
-const Searchbar = ()  => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+const Searchbar = ()  => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+  }
+  return(
+  <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
     <div className="flex flex-row justify-start items-center">
       <FiSearch className="w-5 h-5 ml-4"/>
       <input type="search" placeholder="Search" 
-      value=""
+      value={searchTerm}
       name="search-field"
       autoComplete="off"
       id="search-field"
-      onChange={() => {}}
+      onChange={(e) => {setSearchTerm(e.target.value)}}
       className="flex-1 bg-transparent border-none outline-none p-4 text-white text-base placeholder-gray-500" />
     </div>
   </form>
 );
-
+}
 export default Searchbar;
--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -21,7 +21,7 @@ useEffect( () => {
     return (
         <div className='flex flex-col'>
             <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>Around you
-            <span className='font-blavk'>EG</span>
+            <span className='font-blavk'> EGYPT</span>
             </h2>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.map((song,i) => (
